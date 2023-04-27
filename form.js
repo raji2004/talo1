51ba88af18d6c12cc8bd94ba06f459ba07c97953
raji2004 <kingraj1344@gmail.com>
2023-04-27 21:52:54 +0100

    done

---
+++ form.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form')
+
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData)
+    console.log(data)
+    try {
+        const res = await axios.post('https://server-flax-pi.vercel.app', {...data})
+        console.log(res)
+        return res
+    } catch (error) {
+        console.log(error.response)
+        return error.response
+    }
+
+})
