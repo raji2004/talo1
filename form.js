f7c0dd72f113dd3c753658fcb04a6d0fd8517270
raji2004 <kingraj1344@gmail.com>
2023-04-30 12:06:36 +0100

    done

--- form.js
+++ form.js
@@ -1,15 +1,21 @@
 const form = document.getElementById('form')
-
+const body = document.getElementById('body')
+const button = document.getElementById('container')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
     const data = Object.fromEntries(formData)
+
     console.log(data)
     try {
-        const res = await axios.post('https://server-flax-pi.vercel.app', {...data})
+        const res = await axios.post('https://server-flax-pi.vercel.app', { ...data })
         console.log(res)
-        alert('you have signed up successfully')
+        button.style.display = 'block'
+        body.style.position = 'absolute'
+        body.style.top= 0
+        
+        $("#myModal").modal("toggle");
         return res
     } catch (error) {
         console.log(error.response)
