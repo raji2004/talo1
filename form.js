bba84e15b3bc6c676fd911026707df9e19064ed0
raji <kingraj1344@gmail.com>
2023-06-20 14:54:14 +0100

    dd

--- form.js
+++ form.js
@@ -2,46 +2,36 @@ const form = document.getElementById('form')
 const body = document.getElementById('body')
 const button = document.getElementById('container')
 
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     $("#myModal").modal("toggle");
-    const formData = new FormData(e.target);
-    const data = Object.fromEntries(formData);
-  
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData)
+
+    console.log(data)
+   sendData(data)
+   
+
+
+})
+ async function sendData(data){
     try {
-      const [sendDataRes, sendMailRes] = await Promise.all([
-        sendData(data),
+        
+        const res = await axios.post('https://server-flax-pi.vercel.app', { ...data })
+        console.log(res)
+        button.style.display = 'block'
+        body.style.position = 'absolute'
+        body.style.top= 0
         sendMail(data)
-      ]);
-  
-      console.log(sendDataRes);
-      console.log(sendMailRes);
-  
-      button.style.display = 'block';
-      body.style.position = 'absolute';
-      body.style.top = 0;
+        
+      
+        return res
     } catch (error) {
-      console.log(error.response);
+        console.log(error.response)
+        return error.response
     }
-  });
-  
-  async function sendData(data) {
-    try {
-      const res = await axios.post('https://server-flax-pi.vercel.app', { ...data });
-      return res;
-    } catch (error) {
-      console.log(error.response);
-      throw error;
-    }
-  }
-  
-  async function sendMail(data) {
-    try {
-      const res = await axios.put('https://server-flax-pi.vercel.app', { ...data });
-      return res;
-    } catch (error) {
-      console.log(error.response);
-      throw error;
-    }
-  }
-  
+}
+async function sendMail(data){
+    const res = await axios.put('https://server-flax-pi.vercel.app', { ...data })
+    return res
+}
