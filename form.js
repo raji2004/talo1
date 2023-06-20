197c2048cd7c3b6317c7ba8a62ed4dd90bdf1ea0
raji <kingraj1344@gmail.com>
2023-06-20 14:46:22 +0100

    ff

--- form.js
+++ form.js
@@ -2,35 +2,46 @@ const form = document.getElementById('form')
 const body = document.getElementById('body')
 const button = document.getElementById('container')
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
     $("#myModal").modal("toggle");
-    const formData = new FormData(e.target)
-    const data = Object.fromEntries(formData)
-
-    console.log(data)
-   sendData(data)
-    sendMail(data)
-
-
-})
- async function sendData(data){
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+  
     try {
-        
-        const res = await axios.post('https://server-flax-pi.vercel.app', { ...data })
-        console.log(res)
-        button.style.display = 'block'
-        body.style.position = 'absolute'
-        body.style.top= 0
-        
-      
-        return res
+      const [sendDataRes, sendMailRes] = await Promise.all([
+        sendData(data),
+        sendMail(data)
+      ]);
+  
+      console.log(sendDataRes);
+      console.log(sendMailRes);
+  
+      button.style.display = 'block';
+      body.style.position = 'absolute';
+      body.style.top = 0;
     } catch (error) {
-        console.log(error.response)
-        return error.response
+      console.log(error.response);
     }
-}
-async function sendMail(data){
-    const res = await axios.put('https://server-flax-pi.vercel.app', { ...data })
-    return res
-}
+  });
+  
+  async function sendData(data) {
+    try {
+      const res = await axios.post('https://server-flax-pi.vercel.app', { ...data });
+      return res;
+    } catch (error) {
+      console.log(error.response);
+      throw error;
+    }
+  }
+  
+  async function sendMail(data) {
+    try {
+      const res = await axios.put('https://server-flax-pi.vercel.app', { ...data });
+      return res;
+    } catch (error) {
+      console.log(error.response);
+      throw error;
+    }
+  }
+  
