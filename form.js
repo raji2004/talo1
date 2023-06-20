94f37081214018f494c9f37458618480ea18fa85
raji <kingraj1344@gmail.com>
2023-06-20 13:56:13 +0100

    done

--- form.js
+++ form.js
@@ -4,12 +4,13 @@ const button = document.getElementById('container')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
+    $("#myModal").modal("toggle");
     const formData = new FormData(e.target)
     const data = Object.fromEntries(formData)
 
     console.log(data)
     try {
-        $("#myModal").modal("toggle");
+        
         const res = await axios.post('https://server-flax-pi.vercel.app', { ...data })
         console.log(res)
         button.style.display = 'block'
